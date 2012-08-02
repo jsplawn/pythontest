cee54c72e9a23ba34f3ae6686e7d163a50baa88d
Jsplawn <jsplawn@gmail.com>
2012-08-02 13:09:15 -0700

    finished HW

--- google-python-exercises/basic/string1.py
+++ google-python-exercises/basic/string1.py
@@ -24,11 +24,11 @@
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
-  if count < 10
-    print "Number of donuts: " = count
-  else
-    print "Number of donuts: many"
-  return
+  if count < 10:
+    return "Number of donuts: " + str(count)
+  else:
+    return "Number of donuts: many"
+  
 
 
 # B. both_ends
@@ -37,11 +37,11 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-  if len(s) > 1
-    print s[0:1] + s[-2:-1]
-  else
-    print ""
-  return
+  if len(s) > 1:
+    return s[:2] + s[-2:]
+  else:
+    return ""
+  
 
 
 # C. fix_start
@@ -54,8 +54,8 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-  # +++your code here+++
-  return
+  return s[0] + s[1:].replace(s[0], "*")
+  
 
 
 # D. MixUp
@@ -66,8 +66,8 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-  # +++your code here+++
-  return
+  return b[:2] + a[2:] + " " + a[:2] + b[2:]
+  
 
 
 # Provided simple test() function used in main() to print
